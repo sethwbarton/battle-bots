07c778eb485fa98b7220b0984c59c023e253b292
sethwbarton <sethwbarton@gmail.com>
2022-04-23 23:16:54 -0600

    Controls center around the ship.

--- src/services/three-ui/three_ui_service.ts
+++ src/services/three-ui/three_ui_service.ts
@@ -16,6 +16,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Ship } from '../../entities/ship'
 import { StarSystem } from '../../entities/star_system'
 import { PlayerLocation } from '../../entities/player_location'
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls'
 
 export class ThreeUiService implements UiService {
   private clock: Clock
@@ -33,55 +34,56 @@ export class ThreeUiService implements UiService {
   private controls = new OrbitControls(this.camera, this.renderer.domElement)
 
   constructor() {
-    this.controls.update()
-
     // Adding in a white ambient light
+    console.log('constructor')
     const color = 0xffffff
     const intensity = 1
     const light = new AmbientLight(color, intensity)
     this.scene.add(light)
 
-    this.camera.lookAt(new Vector3(0, 0, 0))
     this.renderer.setSize(innerWidth, innerHeight)
     this.renderer.setClearColor(new Color('rgb(100,100,100)'))
     this.clock = new Clock()
+    this.scene.add(new THREE.AxesHelper(100))
     this.renderGameState(BattleBotsGameState.getInstance())
-    this.render()
   }
 
   public async showStartScreen(): Promise<void> {}
 
   public async renderGameState(gameState: BattleBotsGameState) {
-    this.setCameraToPlayerLocation(gameState.player.location)
+    this.justifyCamera(gameState.player.location)
     this.renderPlayerShip(gameState.player.currentShip)
     this.renderCurrentSystem(gameState.player.location.starSystem)
+    this.render()
   }
 
-  private setCameraToPlayerLocation(playerLocation: PlayerLocation) {
-    this.camera.lookAt(
+  private justifyCamera(playerLocation: PlayerLocation) {
+    this.camera.position.set(
+      playerLocation.coords.x,
+      playerLocation.coords.y + 10,
+      playerLocation.coords.z
+    )
+    this.controls.update()
+    this.controls.target.set(
       playerLocation.coords.x,
       playerLocation.coords.y,
       playerLocation.coords.z
     )
-    this.controls.target.x = playerLocation.coords.x
-    this.controls.target.y = playerLocation.coords.y
-    this.controls.target.z = playerLocation.coords.z
-    this.controls.update()
   }
 
   private renderCurrentSystem(currentStarSystem: StarSystem) {
-    const starGeometry = new THREE.SphereGeometry(10)
-    const starMaterial = new THREE.MeshBasicMaterial({ color: 'yellow' })
-    const star = new Mesh(starGeometry, starMaterial)
-    star.position.set(0, 0, 0)
-    this.scene.add(star)
+    // const starGeometry = new THREE.SphereGeometry(10)
+    // const starMaterial = new THREE.MeshBasicMaterial({ color: 'yellow' })
+    // const star = new Mesh(starGeometry, starMaterial)
+    // star.position.set(0, 0, 0)
+    // this.scene.add(star)
   }
 
   private renderPlayerShip(ship: Ship) {
     const geometry = new THREE.CapsuleGeometry(1, 1, 4, 8)
     const material = new MeshBasicMaterial({ color: 0x00ff00 })
     const capsule = new Mesh(geometry, material)
-    capsule.position.set(ship.coords.x, ship.coords.y - 100, ship.coords.z)
+    capsule.position.set(ship.coords.x, ship.coords.y, ship.coords.z)
     this.scene.add(capsule)
   }
 
@@ -98,6 +100,7 @@ export class ThreeUiService implements UiService {
   private render() {
     this.renderer.render(this.scene, this.camera)
     this.controls.update()
+    this.camera.updateProjectionMatrix()
     requestAnimationFrame(() => this.render())
     this.adjustCanvasSize()
   }
