9c39379d2f35b69c87bc9f14b3dab831bcd56fac
sethwbarton <sethwbarton@gmail.com>
2022-06-04 22:59:19 -0600

    Putting this down for now. Left a note for future me on what to add next.

--- src/game/turn.ts
+++ src/game/turn.ts
@@ -4,6 +4,20 @@ import { Command } from './command'
 import { movePlayer } from './player'
 import { isValidNpc, talkToNpc } from './npc'
 
+/*
+Hey Seth,
+
+Here's the next thing you need to add to your game.
+There needs to be some way to inspect things (the bed) and have a script run.
+Because under the bed there's going to be a sack and in the sack there's going to be a key
+and a mysterious note. Polonius is going to be ticked when he sees it because he was only
+being sassy when he said to check the bed.
+
+So we need a scripting engine... and an inventory for the player....
+and a way to open a door given that you have the right key.
+
+Happy hacking!
+ */
 export async function doPlayerTurn(
   gameState: GameState,
   uiController: UiController
